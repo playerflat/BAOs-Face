04c21eb051594ca893369c80b4635a87eccc3d80
irispon <47470214+irispon@users.noreply.github.com>
2019-05-29 15:54:38 +0900

    [블루투스]연동 완료

--- app/src/main/java/bao/welfare/baoface/MainActivity.java
+++ app/src/main/java/bao/welfare/baoface/MainActivity.java
@@ -1,5 +1,6 @@
 package bao.welfare.baoface;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.ThumbnailUtils;
 import android.provider.MediaStore;
@@ -7,12 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
 
+import bao.welfare.baoface.bluetooth.BluetoothActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent= new Intent(getApplicationContext(),BluetoothActivity.class);
+        startActivity(intent);
         VideoView view = findViewById(R.id.mouse);
         Bitmap thumb = ThumbnailUtils.createVideoThumbnail("file path/url", MediaStore.Images.Thumbnails.MINI_KIND);
 
